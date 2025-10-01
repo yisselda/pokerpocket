99795492f66db35bc55cd978fa8b879075272d13
Yisselda <yisseldar@gmail.com>
2025-10-01 13:00:30 -0400

    action creators for thin UI

---
+++ engine/src/actions.ts
@@ -0,0 +1,29 @@
+import type { Action, SeatId } from './types'
+
+export const startHand = (): Action => ({ type: 'START' })
+export const dealCards = (): Action => ({ type: 'DEAL_CARDS' })
+export const endRound = (): Action => ({ type: 'ROUND_COMPLETE' })
+export const toShowdown = (): Action => ({ type: 'SHOWDOWN' })
+export const nextHand = (): Action => ({ type: 'NEXT_HAND' })
+
+export const fold = (seat: SeatId): Action => ({
+  type: 'PLAYER_ACTION',
+  seat,
+  move: 'FOLD',
+})
+export const check = (seat: SeatId): Action => ({
+  type: 'PLAYER_ACTION',
+  seat,
+  move: 'CHECK',
+})
+export const call = (seat: SeatId): Action => ({
+  type: 'PLAYER_ACTION',
+  seat,
+  move: 'CALL',
+})
+export const raiseTo = (seat: SeatId, amount: number): Action => ({
+  type: 'PLAYER_ACTION',
+  seat,
+  move: 'RAISE',
+  amount,
+})
